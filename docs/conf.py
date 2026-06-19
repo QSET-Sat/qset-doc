b2e994f19fc20f7eb68484fc332d41124e6030a4
jaron <jaron@cyna.com>
2026-06-19 11:53:22 -0400

    Add favicon to documentation site

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_favicon = '_static/favicon.png'
 
 html_css_files = [
     'custom.css',
